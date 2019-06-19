fe32101745031f9d636291092b1eccdf7869cde6
Troy Wang <troy517545@gmail.com>
2019-06-20 06:54:41 +0800

    commit

--- Assets/scripts/Star.cs
+++ Assets/scripts/Star.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Star
 {
-    public double x=0, y=0, z=0;
-    public Star(double X, double Y, double Z)
+    public double x = 0, y = 0, z = 0, vx = 0, vy = 0, vz = 0;
+
+    public Star() { }
+
+    public Star(double X, double Y, double Z, double VX, double VY, double VZ)
     {
         x = X;
         y = Y;
         z = Z;
+        vx = VX;
+        vy = VY;
+        vz = VZ;
     }
     public void print()
     {
-        Debug.Log(x + " " + y + " " + z);
+       
+    }
+
+    public string Info()
+    {
+        string s = "x: " + x + ",   y: " + y + ",   z: " + z + ",   vx: " + vx + ",   vy: " + vy + ",   vz: " + vz;
+        return s;
     }
 }
--- Assets/scripts/test.cs
+++ Assets/scripts/test.cs
@@ -7,27 +7,8 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        double[] arr = { 3, 2, -1, 2, 4, -2, 1, -2, 5};
-        double[] arr1 = { 0.4, 0.4, 1.2 };
-        MAT A = new MAT(3, arr);
-        VEC b = new VEC(3, arr1);
-
-        //for (int i = 0; i < 5; i++)
-        //{
-        //    for(int j = 0; j < 5; j++)
-        //    {
-        //        A[i][j] = i;
-        //    }
-        //    b[i] = i;
-        //}
-        A.print();
-        b.print();
-
-        VEC x = numericalFunctions.LU_Solve(A, b);
-
-        x.print();
-        A.print();
-        (A * x).print();
+        Star s = new Star();
+        Debug.Log(s.Info());
     }
 
 
