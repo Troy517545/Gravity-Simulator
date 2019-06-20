094b34d28818e21523bb45241867287c9fc7f61a
Troy Wang <troy517545@gmail.com>
2019-06-21 03:14:34 +0800

    add camera lock

--- Assets/scripts/cameraMovement.cs
+++ Assets/scripts/cameraMovement.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class cameraMovement : MonoBehaviour
 {
+    private bool cameraRotationLock = false;
+    private bool cameraMovementLock = false;
+
     public float speedH = 3.0f;
     public float speedV = 3.0f;
-
+    
 
     public float walkSpeed = 0.5f;
 
@@ -17,43 +20,54 @@ public class cameraMovement : MonoBehaviour
 
     void Update()
     {
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
+        if (cameraRotationLock == false)
+        {
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+            if (pitch > 85)
+            {
+                pitch = 85;
+            }
+            else if (pitch < -85)
+            {
+                pitch = -85;
+            }
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+        
 
-        if(pitch > 85)
+        if (Input.GetKeyDown("l"))
         {
-            pitch = 85;
+            cameraRotationLock = (cameraRotationLock == false) ? true : false;
+            cameraMovementLock = (cameraMovementLock == false) ? true : false;
         }
-        else if(pitch < -85)
-        {
-            pitch = -85;
-        }
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
-
-        if (Input.GetKey("w"))
+        if (cameraMovementLock == false)
         {
-            transform.position += walkSpeed * (transform.forward).normalized;
-        }
-        if (Input.GetKey("s"))
-        {
-            transform.position -= walkSpeed * (transform.forward).normalized;
-        }
-        if (Input.GetKey("a"))
-        {
-            transform.position -= walkSpeed * (transform.right).normalized;
-        }
-        if (Input.GetKey("d"))
-        {
-            transform.position += walkSpeed * (transform.right).normalized;
-        }
-        if (Input.GetKey("left shift"))
-        {
-            transform.position -= walkSpeed * (Vector3.up).normalized;
-        }
-        if (Input.GetKey("space"))
-        {
-            transform.position += walkSpeed * (Vector3.up).normalized;
+            if (Input.GetKey("w"))
+            {
+                transform.position += walkSpeed * (transform.forward).normalized;
+            }
+            if (Input.GetKey("s"))
+            {
+                transform.position -= walkSpeed * (transform.forward).normalized;
+            }
+            if (Input.GetKey("a"))
+            {
+                transform.position -= walkSpeed * (transform.right).normalized;
+            }
+            if (Input.GetKey("d"))
+            {
+                transform.position += walkSpeed * (transform.right).normalized;
+            }
+            if (Input.GetKey("left shift"))
+            {
+                transform.position -= walkSpeed * (Vector3.up).normalized;
+            }
+            if (Input.GetKey("space"))
+            {
+                transform.position += walkSpeed * (Vector3.up).normalized;
+            }
         }
     }
 }
